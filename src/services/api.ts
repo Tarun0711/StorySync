6442324a283f1e928f0a114f60d10e3d4f4b953a
Helsinki 🧔🏽 <tarunjaat9991@gmail.com>
2025-08-16 08:03:39 +0530

    Render API api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.REACT_APP_API_URL || 'http://localhost:5001/api';
+const API_URL = import.meta.env.REACT_APP_API_URL || 'https://storysync-gszu.onrender.com/api';
 
 export const api = axios.create({
   baseURL: API_URL,
